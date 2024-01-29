5724680631682eabd1d7160cfc7b5aeede4e7aba
mi <33814019+npocmak@users.noreply.github.com>
2024-01-29 20:14:04 +0200

    Update index.js

--- seoConfig/index.js
+++ seoConfig/index.js
@@ -1,12 +1,12 @@
 export default {
-  title: "Logspot - Opensource changelog template",
+  title: "mazafaka's blog",
   description:
-    "Logspot is an open source change log template made with Nuxt, Vue, and Tailwindcss.",
+    "Think Less. Stupid More",
   og: {
-    title: "Logspot - Opensource changelog template",
+    title: "mazafaka's blog",
     description:
-      "Logspot is an open source change log template made with Nuxt, Vue, and Tailwindcss.",
+      "Think Less. Stupid More.",
     image: "https://logspot.vercel.app/logspot-banner.png",
-    url: "https://logspot.vercel.app",
+    url: "https://blog.0x.beer",
   },
 };
